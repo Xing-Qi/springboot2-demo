8f52c6fc173facfe2078020ed46bbe9393dc4c8a
Oliver <Dream_sec@163.com>
2022-11-17 10:35:11 +0800

    second commit

--- boot-admin/src/main/java/com/atguigu/admin/controller/IndexController.java
+++ boot-admin/src/main/java/com/atguigu/admin/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.atguigu.admin.controller;
 
+import com.atguigu.admin.Bean.Account;
+import com.atguigu.admin.Bean.City;
+import com.atguigu.admin.service.AccountService;
+import com.atguigu.admin.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.JdbcProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Oliver
@@ -10,6 +16,35 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    CityService cityService;
+    @ResponseBody
+    @GetMapping("/city")
+    public City getCityByID(@RequestParam("cid") Integer id){
+        return cityService.getCityById(id);
+
+    }
+    @GetMapping("/citymapper")
+    public String index(){
+        return "citymapper";
+    }
+    @ResponseBody
+    @PostMapping("/city")
+    public City insertCity(City city){
+        cityService.insertCity(city);
+        return city;
+    }
+    @ResponseBody
+    @GetMapping("/acct")
+    public Account getAccount(@RequestParam("eid") Integer eid){
+        Account account = accountService.getAccount(eid);
+        return account;
+    }
+
     /**
      * 登录页
      * @return
@@ -34,6 +69,13 @@ public class IndexController {
     @GetMapping("/main.html")
     public String mainPage(){
         return "main";
+    }
+
+    @ResponseBody
+    @GetMapping("/sql")
+    public String queryFromDb(){
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from t_emp", Long.class);
+        return aLong.toString();
     }
 
 }
--- boot-admin/src/test/java/com/atguigu/admin/BootAdminApplicationTests.java
+++ boot-admin/src/test/java/com/atguigu/admin/BootAdminApplicationTests.java
@@ -1,20 +1,37 @@
 package com.atguigu.admin;
 
+import com.atguigu.admin.Bean.Account;
+import com.atguigu.admin.Bean.City;
+import com.atguigu.admin.service.UserService;
+import com.atguigu.admin.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 @Slf4j
 @SpringBootTest
 class BootAdminApplicationTests {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    UserService userService;
     @Test
     void contextLoads() {
         Long object = jdbcTemplate.queryForObject("select count(*) from t_emp", Long.class);
         log.info("记录总数: {}",object);
+        log.info("数据源类型: {}",dataSource.getClass());
+    }
+
+    @Test
+    void getAccount(){
+        Account byId = userService.getById(1);
+        log.info("查询对象:{}",byId);
     }
 
 }
